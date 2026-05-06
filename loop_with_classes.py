b8f2120c0148809f2a29257fc4e6919de7d9a093
Naeem847 <ashoornaeem@gmail.com>
2026-05-06 17:45:50 +0500

    using loop in oop class

---
+++ loop_with_classes.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print("Student name:", self.name)
+
+names = ["Ali", "Sara", "Ahmed"]
+
+students = []
+
+
+for n in names:
+    students.append(Student(n))
+
+
+for s in students:
+    s.show()
