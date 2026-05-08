60bfc99dcd6edb7576687e0b455a5823b77a59b8
Naeem847 <ashoornaeem@gmail.com>
2026-05-08 10:20:17 +0500

    practice the encapsulation concepts of OOP i.e. BankAccount

---
+++ encapsulation.py
@@ -0,0 +1,14 @@
+class BankAccount:
+    def __init__(self,balance):
+        self.__balance=balance #this ia a private variable
+
+    def deposit(self,amount):
+        self.__balance+=amount
+        print("you deposited:",amount)
+    def show_balance(self):
+        print("your balance is:",self.__balance)
+
+account=BankAccount(1000)
+account.deposit(500)
+account.show_balance()
+
