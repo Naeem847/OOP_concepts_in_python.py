285a79b9643021aa81245618c2de5ec52f76373a
Naeem847 <ashoornaeem@gmail.com>
2026-05-24 10:51:35 +0500

    A simple FastAPI project that demonstrates how to create REST API endpoints using Python. The project includes basic GET routes, dynamic path parameters, and server execution with Uvicorn for learning backend API development.

--- oop_cooncepts12.py
+++ oop_cooncepts12.py
@@ -108,3 +108,29 @@ object.show_count()
 
     # def increment(self):
     #     self.count += 1
+    # practise fastapi concepts
+
+
+from fastapi import FastAPI
+ 
+import uvicorn
+
+app= FastAPI(
+    title="My FastAPI Application",
+    description="this is a simple FastAPI application",
+    version="1.0.0"
+
+)
+@app.get("/")   # this handle get request to the root endpoint("/")
+def read_root():
+    
+    return {"message": "Welcome to my FastAPI application!"}
+@app.get("/hello/{name}")  # this handle get request to the "/hello/{name}" endpoint
+
+def read_item(name: str):
+    return {"message": f"Hello, {name}!"}
+
+
+if __name__ == "__main__":
+    # start the server using uvicorn
+    uvicorn.run("my_firstproject:app", host="127.0.0.1", port=8000, reload=True)
