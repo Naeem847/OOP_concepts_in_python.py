9d9354a64b7bcfd6bd31df8942f9b24f3b04ae5d
Naeem847 <ashoornaeem@gmail.com>
2026-06-10 19:53:17 +0500

    practice the inheritance concepts in python

---
+++ inheritance1.py
@@ -0,0 +1,16 @@
+class Animal:
+    def sound(self):
+        print("Animal sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+
+animals = [Dog(), Cat(), Dog()]
+
+for animal in animals:
+    animal.sound()
