1cdcd69342398c7f23d7cf217240c0bbe4a5ca77
Naeem847 <ashoornaeem@gmail.com>
2026-06-10 19:49:18 +0500

    practice the inheritance concepts in python

--- inheritance.py
+++ inheritance.py
@@ -5,5 +5,22 @@ class Animal:
 class Dog(Animal):
     pass
 
+
 d = Dog()
 d.speak()
+
+# # Parent Class
+class Person:
+    def introduce(self):
+        print("I am a Person")
+
+# Child Class
+class Student(Person):
+    pass
+
+# Create a list of students
+students = [Student(), Student(), Student()]
+
+# Use loop
+for s in students:
+    s.introduce()
